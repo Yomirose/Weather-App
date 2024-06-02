73a0ca556d782906f68c2e6bc5bb2b58e676ca30
Yomirose <roselinefayomi10@yahoo.comcleargit config --global user.email roselinefayomi10@yahoo.com>
2024-06-02 23:58:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const searchBtn = document.getElementById('search-btn')
+
+searchBtn.addEventListener('click', function(e) {
+    e.preventDefault
+    const cityInput = document.getElementById('city-input').value;
+    const apiKey = '63d70eb7eac368c79d316d4f78d7865b'; 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(weatherPromise => weatherPromise.json())
+        .then(data => {
+            if (data.cod === '404') {
+                alert('City not found');
+                return;
+            }
+            const weatherInfo = document.getElementById('weather-info');
+            weatherInfo.innerHTML = `
+                <li><strong>Location:</strong> ${data.name}</li>
+                <li><strong>Temperature:</strong> ${data.main.temp} °C</li>
+                <li><strong>Description:</strong> ${data.weather[0].description}</li>
+            `;
+        })
+        .catch(error) 
+});
